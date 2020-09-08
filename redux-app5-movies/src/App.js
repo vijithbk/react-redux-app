d2c06370bfd1f10d039b6c8684a0823348b551c5
Vijith B <vijubalak@gmail.com>
2020-09-08 21:48:14 +0530

    Adding fetch logic

--- redux-app5-movies/src/App.js
+++ redux-app5-movies/src/App.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import './App.css';
 
 import updateMovies from './store/actions/update-movies';
-
+import fetchUsers from './store/actions/fetch-users';
 
 function App(props) {
   return (
@@ -15,19 +15,27 @@ function App(props) {
       >YOUR CURRENT MOVIE IS: </span>{props.movies.name}
       <br />
       <button onClick={props.updateMovies}>SELECT NEW MOVIE</button>
+      <br />
+      {props.users.length === 0 ?
+        <p>THERE ARE NO USERS</p> :
+        props.users.map(user => <p key={user.id}>{user.first_name} -    {user.email}</p>)}
+      <br />
+      <button onClick={props.fetchUsers}>FETCH USERS</button>
     </div >
   );
 };
 
 const MapStateToProps = (state) => {
   return {
-    movies: state.movies
+    movies: state.movies,
+    users: state.users
   };
 };
 
 const MapDispatchToProps = (dispatch) => {
   return {
-    updateMovies: () => dispatch(updateMovies)
+    updateMovies: () => dispatch(updateMovies),
+    fetchUsers: () => dispatch(fetchUsers)
   }
 };
 export default connect(MapStateToProps, MapDispatchToProps)(App);
--- redux-app5-movies/src/store/index.js
+++ redux-app5-movies/src/store/index.js
@@ -1,18 +1,20 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import movieListReducer from './reducers/movies-list';
+import userReducer from './reducers/users';
+import thunk from 'redux-thunk';
+const middleware = [thunk];
 
-const reducer = combineReducers({ movies: movieListReducer });
+const allReducers = combineReducers({ movies: movieListReducer, users: userReducer });
 
 const initialState = {
+    users: [],
     movies: {
         name: "Terminator 2"
     }
 }
 
-const store = createStore(
-    reducer,
-    initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(allReducers, initialState,
+    compose(applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 export default store;
